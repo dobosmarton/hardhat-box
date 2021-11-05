fc5fb868ab86c0a0e5bd5527ecc4ba5c35bebc00
Marton Dobos <dobeis2@gmail.com>
2021-11-05 18:24:32 +0100

    hardhat test with chai

---
+++ scripts/index.js
@@ -0,0 +1,21 @@
+async function main() {
+  const accounts = await ethers.provider.listAccounts();
+  console.log(accounts);
+  // Retrieve accounts from the local node
+  const address = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+  const Box = await ethers.getContractFactory('Box');
+  const box = await Box.attach(address);
+
+  await box.store(42);
+
+  // Call the retrieve() function of the deployed Box contract
+  const value = await box.retrieve();
+  console.log('Box value is', value.toString());
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
